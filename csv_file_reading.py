9a7869a310aa5cb48f6ecfb5cfe0562f1506e472
BRICE JOSHY <bricejoshy99@gmail.com>
2023-05-23 11:42:49 +0530

    csv_file read succesfully

--- csv_file_reading.py
+++ csv_file_reading.py
@@ -1,5 +1,13 @@
 # This file shows the basics of reading data from the csv file
-# : At first, the CSV file is opened using the open() method in ‘r’ mode(specifies read mode while opening a file)
-# which returns the file object then it is read by using the reader() method of CSV module
-# that returns the reader object that iterates throughout the lines in the specified CSV document.
+
+
+import csv
+
 # The ‘with’ keyword is used along with the open() method as it simplifies exception handling and automatically closes the CSV file.
+# At first, the CSV file is opened using the open() method in ‘r’ mode(specifies read mode while opening a file)
+# which returns the file object then it is read by using the reader() method of CSV module
+with open("images.csv", mode="r") as file:
+    csvfile = csv.reader(file)
+    # that returns the reader object that iterates throughout the lines in the specified CSV document.
+    for lines in csvfile:
+        print(lines)
