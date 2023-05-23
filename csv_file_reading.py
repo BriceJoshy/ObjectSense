c967b4ff6db5cd577aca961ed67ab60ccab3922a
BRICE JOSHY <bricejoshy99@gmail.com>
2023-05-23 11:33:32 +0530

    adding the example csv file

---
+++ csv_file_reading.py
@@ -0,0 +1,5 @@
+# This file shows the basics of reading data from the csv file
+# : At first, the CSV file is opened using the open() method in ‘r’ mode(specifies read mode while opening a file)
+# which returns the file object then it is read by using the reader() method of CSV module
+# that returns the reader object that iterates throughout the lines in the specified CSV document.
+# The ‘with’ keyword is used along with the open() method as it simplifies exception handling and automatically closes the CSV file.
