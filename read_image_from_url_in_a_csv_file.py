e2a656e9dcfac248c559b9cdd60b3f3cdbbb41e6
BRICE JOSHY <bricejoshy99@gmail.com>
2023-05-23 12:49:18 +0530

    only reading first csv image

--- read_image_from_url_in_a_csv_file.py
+++ read_image_from_url_in_a_csv_file.py
@@ -12,12 +12,12 @@ with open("images.csv", mode="r") as file:
     for lines in csvfile:
         url = lines[i]
         # using the .get()  from requests to get the data as bytes and raw i.e without any change
-        # response = requests.get(url, stream=True).raw
-        # # .read reads the data into a bytstream and the datatype is changes to uint8
-        # image = np.asarray(bytearray(response.read()), dtype="uint8")
-        # #  reading the image as it is without any change
-        # image = cv.imdecode(image, cv.IMREAD_COLOR)
-        # i = i + 1
-        # cv.imshow("url_image", image)
-        # cv.waitKey(0)
-        # cv.destroyAllWindows()
+        response = requests.get(url, stream=True).raw
+        # .read reads the data into a bytstream and the datatype is changes to uint8
+        image = np.asarray(bytearray(response.read()), dtype="uint8")
+        #  reading the image as it is without any change
+        image = cv.imdecode(image, cv.IMREAD_COLOR)
+        i = i + 1
+        cv.imshow("url_image", image)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
