9441869d4bca0615537c1b34f00d224b46d8caf2
BRICE JOSHY <bricejoshy99@gmail.com>
2023-05-23 12:17:33 +0530

    dataset csv file loaded

--- csv_file_reading.py
+++ csv_file_reading.py
@@ -6,8 +6,11 @@ import csv
 # The ‘with’ keyword is used along with the open() method as it simplifies exception handling and automatically closes the CSV file.
 # At first, the CSV file is opened using the open() method in ‘r’ mode(specifies read mode while opening a file)
 # which returns the file object then it is read by using the reader() method of CSV module
-with open("images.csv", mode="r") as file:
+with open("ima.csv", mode="r") as file:
     csvfile = csv.reader(file)
     # that returns the reader object that iterates throughout the lines in the specified CSV document.
     for lines in csvfile:
-        print(lines)
+        image_name = lines
+        print(image_name)
+
+        #  i is used to print as elements and not as array
---
+++ read_image_from_url_in_a_csv_file.py
@@ -0,0 +1,23 @@
+#  this file shows how to read and show an image from an url in the csv file
+
+import cv2 as cv
+import numpy as np
+import requests
+import csv
+
+with open("images.csv", mode="r") as file:
+    csvfile = csv.reader(file)
+    # that returns the reader object that iterates throughout the lines in the specified CSV document.
+    i = 0
+    for lines in csvfile:
+        url = lines[i]
+        # using the .get()  from requests to get the data as bytes and raw i.e without any change
+        # response = requests.get(url, stream=True).raw
+        # # .read reads the data into a bytstream and the datatype is changes to uint8
+        # image = np.asarray(bytearray(response.read()), dtype="uint8")
+        # #  reading the image as it is without any change
+        # image = cv.imdecode(image, cv.IMREAD_COLOR)
+        # i = i + 1
+        # cv.imshow("url_image", image)
+        # cv.waitKey(0)
+        # cv.destroyAllWindows()
