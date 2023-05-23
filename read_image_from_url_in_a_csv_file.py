b11f986d1d756f74a5e363ef5c338f048fc92de6
BRICE JOSHY <bricejoshy99@gmail.com>
2023-05-23 14:56:32 +0530

    minor changes to the csv immage url display functn

--- read_image_from_url_in_a_csv_file.py
+++ read_image_from_url_in_a_csv_file.py
@@ -5,11 +5,14 @@ import numpy as np
 import requests
 import csv
 
+#  creating a empty list for storing the rows of the single column of the csv file
 image_from_csv_list = []
+#  setting the indexx for the image name so as to prevent the images from crashing while 'imshow' because of the same name
 index = 0
 with open("images.csv", mode="r") as file:
     csvfile = csv.reader(file)
     # that returns the reader object that iterates throughout the lines in the specified CSV document.
+    # for every line adding the column[0] as the list row for the empty list created above
     for lines in csvfile:
         image_from_csv_list.append(lines[0])
 
