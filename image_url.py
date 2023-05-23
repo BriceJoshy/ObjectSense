a0d5d6095f073bb1874b53188261f30f1301a728
BRICE JOSHY <bricejoshy99@gmail.com>
2023-05-23 11:29:23 +0530

    requests package for reading the image from url

--- image_url.py
+++ image_url.py
@@ -3,11 +3,16 @@
 
 import cv2 as cv
 import numpy as np
+
+# package used
 import requests
 
 url = r"https://images.wallpapersden.com/image/download/landscape-pixel-art_bGhnaGeUmZqaraWkpJRtZWWta2Vl.jpg"
+# using the .get()  from requests to get the data as bytes and raw i.e without any change
 response = requests.get(url, stream=True).raw
+# .read reads the data into a bytstream and the datatype is changes to uint8
 image = np.asarray(bytearray(response.read()), dtype="uint8")
+#  reading the image as it is without any change
 image = cv.imdecode(image, cv.IMREAD_COLOR)
 
 cv.imshow("url_image", image)
