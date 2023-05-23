1e2abc2503b63fd5a226e2bd39ba52342ef91725
BRICE JOSHY <bricejoshy99@gmail.com>
2023-05-23 13:06:20 +0530

    reading and showing the first image only

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,1 @@
-response = requests.get(url, stream=True).raw
-        # .read reads the data into a bytstream and the datatype is changes to uint8
-        image = np.asarray(bytearray(response.read()), dtype="uint8")
-        #  reading the image as it is without any change
-        image = cv.imdecode(image, cv.IMREAD_COLOR)
-        i = i + 1
-        cv.imshow("url_image", image)
-        cv.waitKey(0)
-        cv.destroyAllWindows()
+ki"
