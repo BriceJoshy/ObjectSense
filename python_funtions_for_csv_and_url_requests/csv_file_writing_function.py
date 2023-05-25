5c57abade5b96028a48c82c1d101d51cbab49ed3
BRICE JOSHY <bricejoshy99@gmail.com>
2023-05-25 11:11:27 +0530

    csv file creation and writting function

--- python_funtions_for_csv_and_url_requests/csv_file_writing_function.py
+++ python_funtions_for_csv_and_url_requests/csv_file_writing_function.py
@@ -6,8 +6,12 @@ import csv
 
 def csv_file_reading():
     data = ["hi", "hello", "welcome"]
-    with open("output", mode="w", newline="") as file:
-        csvfile = csv.writer(file, lineterminator="\n")
+    with open(
+        "output.csv",
+        mode="w",
+        newline="",
+    ) as file:
+        csvfile = csv.writer(file)
         # that returns the reader object that iterates throughout the lines in the specified CSV document.
         for lines in data:
             csvfile.writerows([lines])
